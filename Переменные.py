cdcdbf561d6bee5fe9829804ac9a986369b69301
ByRoyhoVSA <Forest21@inbox.ru>
2024-09-03 18:18:18 +0300

    Update Переменные.py

--- Переменные.py
+++ Переменные.py
@@ -1,5 +1,5 @@
 Home_Work_complited= 12
 time=1.5
 name="Python"
-time_for_task=0.125
+time_for_task=time/Home_Work_complited
 print(name,",""всего задач:",Home_Work_complited,",""затрачено часов:",time,",""среднее время выполнения:",time_for_task,"часа"".")
