4730c9e33e86b51015ec8357410e7d36aa3bb4f1
ByRoyhoVSA <Forest21@inbox.ru>
2024-09-03 17:54:03 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+Home_Work_complited= 12
+time=1.5
+name="Python"
+time_for_task=0.125
+print(name,",""всего задач:",Home_Work_complited,",""затрачено часов:",time,",""среднее время выполнения:",time_for_task,"часа"".")
